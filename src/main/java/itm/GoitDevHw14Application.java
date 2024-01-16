00c24b0e5578ba21c9ead3d3c517560e9dadeb9e
Itm-info <itm@infotech.net.ua>
2024-01-17 00:31:26 +0200

    Commit

---
+++ src/main/java/itm/GoitDevHw14Application.java
@@ -0,0 +1,10 @@
+package itm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoitDevHw14Application {
+	public static void main(String[] args) { SpringApplication.run(GoitDevHw14Application.class, args);	}
+
+}
