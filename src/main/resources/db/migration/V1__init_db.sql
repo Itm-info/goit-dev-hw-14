c6f7b5337c6bf88345ecb05e9cc0b70b2700df13
Itm-info <itm@infotech.net.ua>
2024-01-17 14:39:27 +0200

    switched to JpaRepository from CrudRepository used saveAndFlush in  public Note add(Note note) updated public List<Note> listAll() to make good use of JpaRepository's findAll method returning List removed all CONSTRAINT and size limits from Migration
    Program does not work, however

--- src/main/resources/db/migration/V1__init_db.sql
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -1,7 +1,5 @@
 CREATE TABLE note (
     id IDENTITY PRIMARY KEY,
-    title VARCHAR(100),
-    content VARCHAR(1000),
-    CONSTRAINT CK_title CHECK ( CHAR_LENGTH(title) > 0 ),
-    CONSTRAINT CK_content CHECK ( CHAR_LENGTH(content) > 0 )
+    title VARCHAR,
+    content VARCHAR
 );
